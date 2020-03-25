812b348cbd47eff6162dd050433e0a72b6b7d2cb
Preeti <preeti09011999@gmail.com>
2020-03-25 20:19:49 +0530

    Search an element in array

---
+++ Lecture 5/searchelement.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+
+public static void main(String[] args) throws Exception {
+    // write your code here
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    int[] arr = new int[n];
+    for(int i=0;i<n;i++){
+        arr[i] = scn.nextInt();
+    }
+    int searchele = scn.nextInt();
+    int count = 0;
+    int pos=-1;
+    for(int j=0;j<n;j++){
+        if(searchele == arr[j]){
+            count ++;
+            pos = j;
+        }
+        
+    }
+    if(count>1){
+        System.out.print(pos);
+    }
+    else{
+        System.out.print(pos);
+    }
+ }
+
+}
