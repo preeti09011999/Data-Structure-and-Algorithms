cddf4e90e894fe36a52c87fa7a6eea51ac760d70
Preeti <preeti09011999@gmail.com>
2020-03-25 20:27:31 +0530

    Rotate an array

---
+++ Lecture 5/rotateArray.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Main{
+  
+
+public static void main(String[] args)  {
+    Scanner scn =  new Scanner(System.in);
+    int n = scn.nextInt();
+    int arr[] = new int[n];
+    
+    for(int i=0;i<arr.length;i++){
+          arr[i] = scn.nextInt();
+      }
+    int k = scn.nextInt();
+    k = k%n;
+    if(k<0){
+        k += n;
+    }
+    int rotation = n - k;
+    int[] narr = new int[n]; 
+    int pos = 0;
+    for(int j=rotation;j<n;j++){
+        narr[pos] = arr[j];
+        pos++;
+    }
+    
+    for(int h=0;h<rotation;h++){
+        narr[pos] = arr[h];
+        pos++;
+    }
+     for(int i=0;i<n;i++){
+        System.out.print(narr[i]+" ");
+    }
+ }
+
+}
