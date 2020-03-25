5edf1a7b4d4147f789b6210ca36b8a19a49eaa97
Preeti <preeti09011999@gmail.com>
2020-03-25 20:25:55 +0530

    Reverse of an array

---
+++ Lecture 5/reverseArray.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+  
+
+public static void main(String[] args) {
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    int[] arr = new int[n];
+    int[] narr = new int[n];
+    for(int i=0;i<n;i++){
+        arr[i] = scn.nextInt();
+    }
+    int last = n-1;
+    int temp;
+    for(int j=0;j<n/2;j++){
+        temp = arr[j];
+        arr[j] = arr[last];
+        arr[last] = temp;
+        last--;
+    }
+    for(int k=0;k<n;k++){
+        System.out.print(arr[k]+" ");
+    }
+ }
+
+}
