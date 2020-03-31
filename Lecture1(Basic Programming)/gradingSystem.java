afb25312e54f204f203b937aa360830ff673d026
Preeti <46680479+preeti09011999@users.noreply.github.com>
2020-03-31 16:38:33 +0530

    grading system problem

--- Lecture1(Basic Programming)/gradingSystem.java
+++ Lecture1(Basic Programming)/gradingSystem.java
@@ -3,7 +3,7 @@ import java.util.*;
   public class Main{
   
   public static void main(String[] args) {
-      // input - don''t change this code
+      
       Scanner scn = new Scanner(System.in);
       int marks = scn.nextInt();
       // input - don''t change this code
