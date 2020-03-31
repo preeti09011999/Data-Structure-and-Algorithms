463fb26045347f3d7eb554ad53426f10750ff55e
Preeti <46680479+preeti09011999@users.noreply.github.com>
2020-03-31 17:01:07 +0530

    Inverse of an array

--- Lecture 5(Functions & Array)/arrayInverse.java
+++ Lecture 5(Functions & Array)/arrayInverse.java
@@ -4,6 +4,7 @@ import java.util.*;
 public class Main{
   
 public static void main(String[] args) {
+  
     Scanner scn = new Scanner(System.in);
       int n = scn.nextInt();
       int arr[] = new int[n];
