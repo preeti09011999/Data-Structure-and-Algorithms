0a3b12717c34a7a6eea588010a74d5ff92a0bd32
Preeti <46680479+preeti09011999@users.noreply.github.com>
2020-04-02 21:31:28 +0530

    Subset of an array

--- Lecture 5(Functions & Array)/subset.java
+++ Lecture 5(Functions & Array)/subset.java
@@ -10,7 +10,7 @@ public class Main{
     }
 
 public static void main(String[] args) throws Exception {
-    // write your code here
+   
     Scanner scn = new Scanner(System.in);
     int n = scn.nextInt();
     int[] arr = new int[n];
