495f6e81a46d01b7fd364bb83f248b5a2ad15566
Preeti <46680479+preeti09011999@users.noreply.github.com>
2020-05-20 07:42:33 +0530

    Payment in installments (Permutations)

---
+++ Lecture 7(Recursion)/payment_in_installments_permutations.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int denominations[] = new int[n];
+        for(int i=0;i<n;i++){
+            denominations[i] = scn.nextInt();
+        }
+        int amt = scn.nextInt();
+        piCombination(denominations,amt,0,"");
+        
+    }
+    static int counter = 0;
+   //lpi=> index of last payment made 
+    public static void piCombination(int denominations[], int amt,int lpi,String asf){
+        if(amt == 0){
+            counter++;
+            System.out.println(counter+" -> " + asf);
+            return;
+        }
+        
+        for(int inst=0;inst<denominations.length;inst++){
+            if(amt>=denominations[inst]){
+                piCombination(denominations,amt-denominations[inst],inst,asf+denominations[inst]+" ");
+            }
+        }
+    }
+    
+}
