797cd1bb75b70a5524d171ef64c2564804debf8d
Preeti <46680479+preeti09011999@users.noreply.github.com>
2020-05-25 21:15:42 +0530

    Add files via upload

---
+++ stacks/balanced_Brackets.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        String str = scn.nextLine();
+        System.out.println(balancedB(str));
+    }
+    public static boolean balancedB(String str){
+        Stack<Character> st = new Stack<>();
+        for(int i=0;i<str.length();i++){
+            char res = str.charAt(i);
+            if(res == '('||res == '{'||res == '['){
+                st.push(res);
+            }
+            else if(res == ')'){
+                if(st.size()==0||st.peek()!='('){
+                    return false;
+                }else{
+                    st.pop();
+                }
+            }
+            else if(res == '}'){
+                if(st.size()==0||st.peek() != '{'){
+                   return false;
+                }else{
+                   st.pop();
+                }
+            }
+            else if(res == ']'){
+                if(st.size()==0||st.peek() != '['){
+                    return false;
+                }else{
+                    st.pop();
+                }
+            }
+    }if(st.size()>0){
+        return false;
+    }
+    
+    return true;
+    
+        
+    }
+}
---
+++ stacks/duplicate_brackets.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        String str = scn.nextLine();
+        System.out.println(dp(str));
+    }
+    
+    public static boolean dp(String str){
+        Stack<Character> st = new Stack<>();
+ 
+        for(int i=0;i<str.length();i++){
+            char res = str.charAt(i);
+            if(res == ')'){
+                if(st.peek()=='('){
+                    return true;
+                }else{
+                    while(st.peek()!='('){
+                        st.pop();
+                    }
+                    st.pop();
+                }
+        
+            }else{
+                st.push(res);
+            }
+        }
+        return false;
+    }
+
+}
