83b1aa542200aafb6b69d1904dc5e20a072613c5
Preeti <46680479+preeti09011999@users.noreply.github.com>
2020-06-05 19:21:00 +0530

    Count valleys And Mountains

--- Dynamic Programming/countValleysAndMountains.java
+++ Dynamic Programming/countValleysAndMountains.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 public class Main{
 
-public static void main(String[] args) throws Exception {
+public static void main(String[] args) throws Exception { 
     Scanner scn = new Scanner(System.in);
     int n = scn.nextInt();
     int ans = binCoeff(2*n,n);
