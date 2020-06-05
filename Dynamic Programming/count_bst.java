a7a1fe5b80ced10bd19209d3ac62206369a3690f
Preeti <46680479+preeti09011999@users.noreply.github.com>
2020-06-05 19:18:12 +0530

    Count Binary Search Tree

--- Dynamic Programming/count_bst.java
+++ Dynamic Programming/count_bst.java
@@ -6,7 +6,7 @@ public class Main{
 public static void main(String[] args) throws Exception {
     Scanner scn = new Scanner(System.in);
     int n = scn.nextInt();
-    int dp[] = new int[n+1];
+    int dp[] = new int[n+1]; 
     dp[0] = 1;
     for(int i=1;i<=n;i++){
         for(int j=0;j<i;j++){
