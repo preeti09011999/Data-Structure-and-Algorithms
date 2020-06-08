ebd989d0ab4c74aab6b89f2a656e0e82f4f4326d
Preeti <46680479+preeti09011999@users.noreply.github.com>
2020-06-08 14:31:31 +0530

    Add files via upload

---
+++ Generic Tree/levelOrder_linewise_zigzag.java
@@ -0,0 +1,142 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+  private static class Node {
+    int data;
+    ArrayList<Node> children = new ArrayList<>();
+  }
+
+  public static void display(Node node) {
+    String str = node.data + " -> ";
+    for (Node child : node.children) {
+      str += child.data + ", ";
+    }
+    str += ".";
+    System.out.println(str);
+
+    for (Node child : node.children) {
+      display(child);
+    }
+  }
+
+  public static Node construct(int[] arr) {
+    Node root = null;
+
+    Stack<Node> st = new Stack<>();
+    for (int i = 0; i < arr.length; i++) {
+      if (arr[i] == -1) {
+        st.pop();
+      } else {
+        Node t = new Node();
+        t.data = arr[i];
+
+        if (st.size() > 0) {
+          st.peek().children.add(t);
+        } else {
+          root = t;
+        }
+
+        st.push(t);
+      }
+    }
+
+    return root;
+  }
+
+  public static int size(Node node) {
+    int s = 0;
+
+    for (Node child : node.children) {
+      s += size(child);
+    }
+    s += 1;
+
+    return s;
+  }
+
+  public static int max(Node node) {
+    int m = Integer.MIN_VALUE;
+
+    for (Node child : node.children) {
+      int cm = max(child);
+      m = Math.max(m, cm);
+    }
+    m = Math.max(m, node.data);
+
+    return m;
+  }
+
+  public static int height(Node node) {
+    int h = -1;
+
+    for (Node child : node.children) {
+      int ch = height(child);
+      h = Math.max(h, ch);
+    }
+    h += 1;
+
+    return h;
+  }
+
+  public static void traversals(Node node){
+    System.out.println("Node Pre " + node.data);
+
+    for(Node child: node.children){
+      System.out.println("Edge Pre " + node.data + "--" + child.data);
+      traversals(child);
+      System.out.println("Edge Post " + node.data + "--" + child.data);
+    }
+
+    System.out.println("Node Post " + node.data);
+  }
+
+  public static void levelOrderLinewiseZZ(Node node){
+    Stack<Node> mainS = new Stack<>();
+    Stack<Node> childS = new Stack<>();
+    int level = 0;
+    mainS.push(node);
+    while(mainS.size()>0){
+        // remove
+        Node temp = mainS.pop();
+        // print
+        System.out.print(temp.data+" ");
+        // add
+        // if level is even....then apply increasing for loop to print in clockwise direction
+        if(level % 2 == 0){
+            for(int i=0;i<temp.children.size();i++){
+                Node child = temp.children.get(i); 
+                childS.push(child);
+            }
+        }
+        // if level is odd..i.e print the child in anticlockwise order
+        else{
+            for(int i=temp.children.size()-1;i>=0;i--){
+                Node child = temp.children.get(i);
+                childS.push(child);
+            }
+        }
+        
+        if(mainS.size()==0){
+            mainS = childS;
+            childS = new Stack<>();
+            level++;
+            System.out.println();
+        }
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
+    int[] arr = new int[n];
+    String[] values = br.readLine().split(" ");
+    for (int i = 0; i < n; i++) {
+      arr[i] = Integer.parseInt(values[i]);
+    }
+
+    Node root = construct(arr);
+    levelOrderLinewiseZZ(root);
+  }
+
+}
