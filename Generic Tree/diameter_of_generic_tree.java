c61e021ccd91033b4f4b2e0224d876bc41f565d5
Preeti <46680479+preeti09011999@users.noreply.github.com>
2020-06-14 23:11:20 +0530

    Diameter of  a generic tree

--- Generic Tree/diameter_of_generic_tree.java
+++ Generic Tree/diameter_of_generic_tree.java
@@ -23,7 +23,7 @@ public class Main {
   public static Node construct(int[] arr) {
     Node root = null;
 
-    Stack<Node> st = new Stack<>();
+    Stack<Node> st = new Stack<>(); 
     for (int i = 0; i < arr.length; i++) {
       if (arr[i] == -1) {
         st.pop();
