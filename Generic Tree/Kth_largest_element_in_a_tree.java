b903aeb16495863c8010d12c47cf02a5b900236b
Preeti <46680479+preeti09011999@users.noreply.github.com>
2020-06-14 23:10:20 +0530

    Kth largest element in a tree

--- Generic Tree/Kth_largest_element_in_a_tree.java
+++ Generic Tree/Kth_largest_element_in_a_tree.java
@@ -13,7 +13,7 @@ public class Main {
       str += child.data + ", ";
     }
     str += ".";
-    System.out.println(str);
+    System.out.println(str); 
 
     for (Node child : node.children) {
       display(child);
