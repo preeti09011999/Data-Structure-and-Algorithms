4ef20bc12271ea4cb843d86f3359d1c1906ed70b
Preeti <46680479+preeti09011999@users.noreply.github.com>
2020-06-19 18:28:31 +0530

    You are required to find the intersection of array1 and array2
    if a1 -> 1 1 2 2 2 3 5
    and a2 -> 1 1 1 2 2 4 5
    intersection is -> 1 1 2 2 5

--- Hashmaps and Heaps/get_common_elements(Intersection).java
+++ Hashmaps and Heaps/get_common_elements(Intersection).java
@@ -5,7 +5,7 @@ public class Main{
 
 public static void main(String[] args) throws Exception {
     Scanner scn = new Scanner(System.in);
-    int n1= scn.nextInt();
+    int n1= scn.nextInt(); 
     int a1[] = new int[n1];
     for(int i=0;i<n1;i++){
         a1[i] = scn.nextInt();
