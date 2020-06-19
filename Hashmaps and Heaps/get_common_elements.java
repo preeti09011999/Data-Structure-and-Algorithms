284a4bdf6067aec0bfb5468870b159c3fad7bd68
Preeti <46680479+preeti09011999@users.noreply.github.com>
2020-06-19 18:32:22 +0530

    You are required to print all elements of array2 which are also present in array1 (in order of their occurence in array2).

--- Hashmaps and Heaps/get_common_elements.java
+++ Hashmaps and Heaps/get_common_elements.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.util.*;
 
 public class Main{
-
+ 
 public static void main(String[] args) throws Exception {
     Scanner scn = new Scanner(System.in);
     int n1 = scn.nextInt();
