647e5d0b46fc592ef08db79fcdfec88aacaac159
Preeti <46680479+preeti09011999@users.noreply.github.com>
2020-06-19 18:33:46 +0530

    Longest Consecutive Sequence

--- Hashmaps and Heaps/longest_consecutive_sequence.java
+++ Hashmaps and Heaps/longest_consecutive_sequence.java
@@ -4,7 +4,7 @@ import java.util.*;
 public class Main{
 
 public static void main(String[] args) throws Exception {
-    Scanner scn = new Scanner(System.in);
+    Scanner scn = new Scanner(System.in); 
     int n = scn.nextInt();
     int a[] = new int[n];
     for(int i=0;i<n;i++){
