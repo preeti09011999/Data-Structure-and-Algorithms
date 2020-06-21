8431abea8aa58e06ce31de6cc73398bd0a1057a5
Preeti <46680479+preeti09011999@users.noreply.github.com>
2020-06-21 22:18:43 +0530

    Add files via upload

---
+++ Priority Queue/contruct_PQ.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main{
+
+public static void main(String[] args) {
+
+// write your code here.
+    PriorityQueue<String> pq = new PriorityQueue<>();
+    pq.add("Aaman");
+    pq.add("sgjh");
+    pq.add("Kal");
+    pq.add("khs");
+    pq.add("hjsjgh");
+    pq.add("klkgzhhg");
+    pq.add("nnsjhhgghhgjss");
+    pq.add("ahaggshjhjkkjjkjkkj");
+    System.out.println("Size: "+pq.size());
+    while(pq.size()>0){
+        System.out.println(pq.remove());
+    }
+
+ }
+}
